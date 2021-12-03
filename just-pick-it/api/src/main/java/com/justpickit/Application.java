e4d19d0357c4e3864698ddc6890f16de333f4cab
TassioCosta <tassio2077@gmail.com>
2021-12-03 00:56:59 -0300

    Update

--- just-pick-it/api/src/main/java/com/justpickit/Application.java
+++ just-pick-it/api/src/main/java/com/justpickit/Application.java
@@ -1,6 +1,6 @@
 package com.justpickit;
 
-//Git Token: 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
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
